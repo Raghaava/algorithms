8a70e750ed2947544a6c675cbd681f914576bc37
Raghaava <javvaji.raghava@gmail.com>
2019-07-22 12:54:26 -0700

    finding median from stream using heaps

--- heap/FindMedianInStream.py
+++ heap/FindMedianInStream.py
@@ -1,27 +1,32 @@
 import MaxHeap
-
+import MinHeap
 
 min_heap = []
 max_heap = []
 
 current_median = 0.0
 
-stream = [1, 2, 3, 4, 5]
+stream = [6, 1, 5, 2, 4, 3, 1, 7, 7, 8]
 
 for value in stream:
-    if current_median < value:
+    if current_median > value:
         MaxHeap.push_heap(max_heap, value)
     else:
-        MaxHeap.push_heap(min_heap, value)
+        MinHeap.push_heap(min_heap, value)
 
-    if len(min_heap)-len(max_heap) > 1:
-        min_value = MaxHeap.pop_heap(min_heap)
-        MaxHeap.push_heap(max_heap, min_value)
-        current_median = min_heap[0]
-    elif len(max_heap)-len(min_heap) > 1:
+    current_median = value
+
+    if len(max_heap)-len(min_heap) > 1:
         max_value = MaxHeap.pop_heap(max_heap)
         MaxHeap.push_heap(min_heap, max_value)
+    elif len(min_heap)-len(max_heap) > 1:
+        min_value = MinHeap.pop_heap(min_heap)
+        MinHeap.push_heap(max_heap, min_value)
+
+    if len(max_heap) == len(min_heap):
+        current_median = (max_heap[0]+min_heap[0])/2
+    elif len(max_heap) > len(min_heap):
         current_median = max_heap[0]
-    elif len(max_heap) == 1 and
-    current_median = (max_heap[0]+min_heap[0])/2
-    print(current_median)
+    else:
+        current_median = min_heap[0]
+    print(current_median, max_heap, min_heap)
